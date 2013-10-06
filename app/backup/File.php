4bfca4ab8bae4f644b24d828e543c5b68f098439
Adam Malone <adam@adammalone.net>
2013-10-06 19:38:06 +1100

    Made one of the files methods static and instantiated the array correctly

--- app/backup/File.php
+++ app/backup/File.php
@@ -13,8 +13,9 @@ namespace backup;
 class File {
   public $files = array();
 
-  public function loadFiles($path, $mask) {
+  public static function loadFiles($path, $mask) {
     if (is_dir($path) && $handle = @opendir($path)) {
+      $files = array();
       while (FALSE !== ($filename = readdir($handle))) {
         if ($filename != '.' && preg_match($mask, $filename)) {
           $uri = "$path/$filename";
@@ -28,9 +29,10 @@ class File {
       }
       return $files;
     }
+    return array();
   }
 
-  public function checkDirectory($path) {
+  public static function checkDirectory($path) {
     if (is_dir($path) && is_writeable($path)) {
       return TRUE;
     }
