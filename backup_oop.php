52d6fc7350136366ce6d73e882e96c367ff7c31f
Adam Malone <adam@adammalone.net>
2014-10-17 02:12:00 +1100

    Altered composer packages and the autoloader

--- backup_oop.php
+++ backup_oop.php
@@ -2,7 +2,8 @@
 <?php
 
 define('ROOT_DIR', dirname(__FILE__));
-require_once ROOT_DIR . "/includes/autoload.inc";
+
+require_once 'vendor/autoload.php';
 require_once ROOT_DIR . "/includes/bootstrap.inc";
 
 try {
